685ebea757027c415cd5b4562c4f11880def1346
Derin DeNio <derindenio@gmail.com>
2024-02-20 14:32:12 -0700

    Completed Step 3.4; Added Delete request, confirmed delete functionality on Postman

--- server.js
+++ server.js
@@ -6,13 +6,16 @@
 
 // Screenshot for 3.2
 // The page has to show the time that the request was received and how many entries are in the phonebook at the time of processing the request.
-
 // There can only be one response.send() statement in an Express app route. Once you send a response to the client using response.send(), the request-response cycle is complete and no further response can be sent.
 
 // 3.3: Phonebook backend step 3
 // Implement the functionality for displaying the information for a single phonebook entry. The url for getting the data for a person with the id 5 should be http://localhost:3001/api/persons/5
-
 // If an entry for the given id is not found, the server has to respond with the appropriate status code.
+
+// 3.4: Phonebook backend step 4
+// Implement functionality that makes it possible to delete a single phonebook entry by making an HTTP DELETE request to the unique URL of that phonebook entry.
+// Test that your functionality works with either Postman or the Visual Studio Code REST client.
+
 const express = require('express')
 const app = express()
 
@@ -64,7 +67,14 @@ app.get('/api/persons/:id', (request, response) => {
     } else {
       response.status(404).end()
     }
-  })
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+  
+    response.status(204).end()
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
